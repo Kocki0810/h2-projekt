39ad656d28c009eefd72497e52dbef8c98c3ae05
Mads <madsi2000@gmail.com>
2020-01-07 14:50:16 +0100

    update

--- h2 nibe project/h2 nibe project/Program.cs
+++ h2 nibe project/h2 nibe project/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 namespace h2_nibe_project
 {
-
+    #region data controllers etc.
     class Stands
     {
         public string BodId { get; set; }
@@ -74,50 +74,119 @@ namespace h2_nibe_project
             setup.AddStand(stand);
         }
     }
+    #endregion
     class UserInput
     {
         protected int output;
         SetupController Controller = new SetupController();
         protected string input;
-        public void Start()
+        public void CheckForNumber()
         {
-            Console.WriteLine("Press 1 to create or 2 to show");
-            input = Console.ReadLine();
-
-            Console.WriteLine("Press 1 to create a stand and press 2 to create a switch");
-            input = Console.ReadLine();
             while(!Int32.TryParse(input, out output))
             {
                 Console.WriteLine("Must be a number");
                 input = Console.ReadLine();
-
             }
+        }
+        public void Start()
+        {
+            Console.WriteLine("Press 1 to create or 2 to show");
+            input = Console.ReadLine();
+            CheckForNumber();
             if(input == "1")
             {
-                ShowAddStand();
+                input = "";
+                Console.WriteLine("Press 1 to create a stand or press 2 to create a switch");
+                input = Console.ReadLine();
+                if(input == "1")
+                {
+                    ShowAddStand();
+                }
+                else if(input == "2")
+                {
+                    ShowAddSwitch();
+                }
             }
-            Console.ReadKey();
+            else if(input == "2")
+            {
+                input = "";
+                Console.WriteLine("Press 1 to show stands or press 2 to show switches");
+                input = Console.ReadLine();
+                if(input == "1")
+                {
+                    ShowStands();
+                }
+                else if(input == "2")
+                {
+                    ShowSwitch();
+                }
+            }
+            Console.ReadKey(true);
+            Start();
         }
         public void ShowAddStand()
         {
-
-            Console.WriteLine("Write the following with your own input/n");
-            Console.WriteLine("Name, Id, amount of switches, id of the switches(this should be comma seperated without spaces if more than 1");
+            Console.WriteLine("Write the following with your own input");
+            Console.WriteLine("Id, name, amount of switches, id of the switches(this should be comma seperated without spaces if more than 1");
             string standInfo = Console.ReadLine();
-
+            string[] idSplit = { "" };
             string[] standSplit = standInfo.Split(" ", 4);
-            string[] idSplit = standSplit[3].Split(",");
+            try
+            {
+                idSplit = standSplit[3].Split(",");
+                Controller.CreateStand(standSplit[0], standSplit[1], standSplit[2], idSplit);
 
-            Controller.CreateStand(standSplit[0], standSplit[1], standSplit[2], idSplit);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Not enough parameters");
+            }
+
         }
-        public void show_stands()
+        public void ShowAddSwitch()
         {
+            Console.WriteLine("Write the following with your own input");
+            Console.WriteLine("Id, amount of ports");
+
+            string switchInfo = Console.ReadLine();
+
+            try
+            {
+                string[] switchSplit = switchInfo.Split(" ", 2);
+                Controller.CreateSwitch(switchSplit[0], switchSplit[1]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Not enough parameters");
+            }
+        }
+        public void ShowRemoveSwitch()
+        {
+            List<NetworkSwitch> @switch = Controller.setup.RemoveSwitch();
+
+        }
+        public void ShowStands()
+        {
+            string id = "";
+      
             List<Stands> stand = Controller.setup.GetStands();
             foreach(Stands i in stand)
             {
-                Console.WriteLine(i);
+                foreach(string x in i.SwitchId)
+                {
+                    id += x + " ";
+                }
+                Console.WriteLine(i.BodId + " " + i.Name + " " + i.SwitchAmount + " " + id);
+                id = "";
+            }
+        }
+        public void ShowSwitch()
+        {
+            List<NetworkSwitch> @switch = Controller.setup.GetSwitches();
+            foreach (NetworkSwitch i in @switch)
+            {
+                Console.WriteLine(i.SwitchId + " " + i.PortAmount);
             }
         }
     }
-  
 }
